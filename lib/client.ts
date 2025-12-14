65912cc88cb0d2b767a4a73291f3ce58b3d0cb86
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-12-14 18:19:51 +0300

    Fix types error

--- lib/client.ts
+++ lib/client.ts
@@ -2,4 +2,4 @@ import { treaty } from '@elysiajs/eden'
 import type { App } from '../app/api/[[...slugs]]/route'
 
 // .api to enter /api prefix
-export const client = treaty<App>(process.env.NEXT_PUBLIC_API_URL).api
+export const client = treaty<App>(String(process.env.NEXT_PUBLIC_API_URL)).api
